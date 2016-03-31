99fc5c4088dffd73fc4418646e2e49ec3f83485d
lolnationCH <canadiens87>
2016-03-30 23:23:51 -0400

    Tests Finish

--- BIgEnum/main.cpp
+++ BIgEnum/main.cpp
@@ -3,10 +3,17 @@
 
 
 int main(){
-	B_int c(58788787);
-	B_int temp(58788787);
+	B_int c("587887875878878788888888888888888888888888888888888888888888587887875878878788888888888888888888888888888888888888888888");
+	B_int temp("587887875878878788888888888888888888888888888888888888888888587887875878878788888888888888888888888888888888888888888888");
+	clock_t t;
+	t = clock();
 	std::cout << c.SchonhageStrassen(c, temp) << std::endl;
-	std::cout << c * temp;
+	t = clock() - t;
+	std::cout << ((float)t) / CLOCKS_PER_SEC << std::endl;
+	t = clock();
+	std::cout << c * temp << std::endl;
+	t = clock() - t;
+	std::cout << ((float)t) / CLOCKS_PER_SEC << std::endl;
 
 	std::cin.get();
 	return 0;
